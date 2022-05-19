a097f351bb08cd3aea87dfb12cdc0a0584dbce8f
shizuka1997 <system.study.1997@gmail.com>
2022-05-19 21:07:33 +0900

    edit commit

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('posts', 'PostController');
- 
+// Route::resource('posts', 'PostController');
+// Route::resource('posts', 'PostaaaController');
+Route::get('posts_001', 'PostaaaController@index');
+Route::get('posts_create', 'PostaaaController@create');
+Route::post('posts_001', 'PostaaaController@store');
+Route::get('posts_001/{post}', 'PostaaaController@show');
+Route::get('posts_001/{post}/edit', 'PostaaaController@edit');
+Route::post('posts_001_update/{post}', 'PostaaaController@update');
+Route::post('posts_001_delete/{post}', 'PostaaaController@destroy');
+
 // if (env('APP_ENV') === 'local') {
 //    URL::forceScheme('https');
 // }
+
+// ->name(posts.show);
